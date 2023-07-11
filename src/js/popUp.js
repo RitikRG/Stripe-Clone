2026c8d7c47c3cb1cd8969fe6eb42eb216c54fd3
Ritik Gupta <ritik.mpm@gmail.com>
2023-07-11 14:07:56 +0530

    Navbar Section Completed

--- src/js/popUp.js
+++ src/js/popUp.js
@@ -4,6 +4,11 @@ const productPopUp= document.querySelector('#productsPopupContainerOut');
 const solutionLink= document.querySelector('#solutionLink');
 const solutionPopUp= document.querySelector('#solutionsPopupContainerOut');
 
+const documentationLink= document.querySelector('#documentationLink');
+const documentationPopUp= document.querySelector('#documentationPopupContainerOut');
+
+const resourcesLink= document.querySelector('#resourcesLink');
+const resourcesPopUp= document.querySelector('#resourcePopupContainerOut');
 
 // Product popup function
 
@@ -16,7 +21,10 @@ productLink.addEventListener('mouseenter', ()=>{
     // removing other popups
     solutionPopUp.classList.add('popUpClosed');
     solutionPopUp.classList.remove('popUpOpen');
-
+    documentationPopUp.classList.add('popUpClosed');
+    documentationPopUp.classList.remove('popUpOpen');
+    resourcesPopUp.classList.add('popUpClosed');
+    resourcesPopUp.classList.remove('popUpOpen');
 })
 
 productPopUp.addEventListener('mouseleave', ()=>{
@@ -37,6 +45,10 @@ solutionLink.addEventListener('mouseenter', ()=>{
     // removing other popups
     productPopUp.classList.add('popUpClosed');
     productPopUp.classList.remove('popUpOpen');
+    documentationPopUp.classList.add('popUpClosed');
+    documentationPopUp.classList.remove('popUpOpen');
+    resourcesPopUp.classList.add('popUpClosed');
+    resourcesPopUp.classList.remove('popUpOpen');
 
 })
 
@@ -44,4 +56,52 @@ solutionPopUp.addEventListener('mouseleave', ()=>{
     solutionPopUp.classList.remove('popUpOpen');
     solutionPopUp.classList.add('popupCLose');
     setTimeout(()=>{solutionPopUp.classList.add('popUpClosed');}, 250);
+})
+
+// Documentation popup functions
+
+documentationLink.addEventListener('mouseenter', ()=>{
+    documentationPopUp.classList.toggle('popUpClosed');
+    documentationPopUp.classList.remove('popupCLose');
+
+    documentationPopUp.classList.add('popUpOpen');
+
+    // removing other popups
+    productPopUp.classList.add('popUpClosed');
+    productPopUp.classList.remove('popUpOpen');
+    solutionPopUp.classList.add('popUpClosed');
+    solutionPopUp.classList.remove('popUpOpen');
+    resourcesPopUp.classList.add('popUpClosed');
+    resourcesPopUp.classList.remove('popUpOpen');
+
+})
+
+documentationPopUp.addEventListener('mouseleave', ()=>{
+    documentationPopUp.classList.remove('popUpOpen');
+    documentationPopUp.classList.add('popupCLose');
+    setTimeout(()=>{documentationPopUp.classList.add('popUpClosed');}, 250);
+})
+
+// Resouces Popup Functions
+
+resourcesLink.addEventListener('mouseenter', ()=>{
+    resourcesPopUp.classList.toggle('popUpClosed');
+    resourcesPopUp.classList.remove('popupCLose');
+
+    resourcesPopUp.classList.add('popUpOpen');
+
+    // removing other popups
+    productPopUp.classList.add('popUpClosed');
+    productPopUp.classList.remove('popUpOpen');
+    solutionPopUp.classList.add('popUpClosed');
+    solutionPopUp.classList.remove('popUpOpen');
+    documentationPopUp.classList.add('popUpClosed');
+    documentationPopUp.classList.remove('popUpOpen');
+
+})
+
+resourcesPopUp.addEventListener('mouseleave', ()=>{
+    resourcesPopUp.classList.remove('popUpOpen');
+    resourcesPopUp.classList.add('popupCLose');
+    setTimeout(()=>{resourcesPopUp.classList.add('popUpClosed');}, 250);
 })
